372bc794a3b83d0e11c0f167fbbfa214fb336033
Nikolay Mamashin <mamashin@gmail.com>
2023-03-02 08:22:08 +0300

    Check auth in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
+import {authStore} from 'src/stores/auth'
 
 /*
  * If not building with SSR mode, you can
@@ -24,6 +25,17 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
+  })
+
+  Router.beforeEach((to, from, next) => {
+    const requiredAuth = to.matched.some(record => record.meta['auth'])
+    const isLogin = authStore().isLoggedIn
+    if (requiredAuth && !isLogin) {
+      next('/login')
+    }
+    else {
+      next()
+    }
   })
 
   return Router
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '', meta: {auth: true} , component: () => import('pages/IndexPage.vue') },
       { path: '/login', component: () => import('pages/LoginPage.vue') },
       { path: '/logout', component: () => import('pages/LogoutPage.vue') }
     ]
